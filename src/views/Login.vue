d72a36572974be2be81e8c3a81225ee19fc2dd59
zskenny <zskenny@gmail.com>
2019-11-08 18:37:19 +0800

    安装插件，更改配置

---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="login">
+        LOGIN
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.login {
+    background-color: #2b4b6b;
+}
+</style>
